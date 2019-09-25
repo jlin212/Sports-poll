02d58e8316c0e3a530870d96ffdda69ed95ea972
jenkins <jenkins@jaguar-team.com>
2019-09-25 15:23:06 +0000

    Yhe files .gitignore and server.js were updated

--- server.js
+++ server.js
@@ -2,15 +2,22 @@
 'use strict';
 
 /**
- * Use `server.js` to run your application without `$ strapi start`.
- * To start the server, run: `$ npm start`.
- *
- * This is handy in situations where the Strapi CLI is not relevant or useful.
- */
+* Use `server.js` to run your application without `$ strapi start`.
+* To start the server, run: `$ npm start`.
+*
+* This is handy in situations where the Strapi CLI is not relevant or useful.
+*/
+
 
 process.chdir(__dirname);
 
+/*
 (() => {
-  const strapi = require('strapi');
-  strapi.start();
+ const strapi = require('strapi');
+ strapi.start();
 })();
+*/
+
+const strapi = require('strapi');
+
+strapi(/* {...} */).start()
